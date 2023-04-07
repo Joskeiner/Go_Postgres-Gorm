62a3881fb217c2a7a2fa3ec2c143835d771713c7
joskeiner <joskeiner.simosa9@gmail.com>
2023-04-07 01:37:15 -0300

    implementacion de funcion para enviar struct

--- models/Book.go
+++ models/Book.go
@@ -1,16 +1,20 @@
-package moels 
+package models
 
-Import "gorm.io/gorm"
+import "gorm.io/gorm"
 
-// modelo o esquema donde se manejara la informacion a guardar o extraer 
+// modelo o esquema donde se manejara la informacion a guardar o extraer
 type Books struct {
-
-	ID  uint `gorm:"primary key; autoIncrement"  json:"id"` 
+	ID uint `gorm:"primary key; autoIncrement"  json:"id"`
 
 	Author *string `json:"author"`
 
 	Title *string `json:"title"`
 
-	Publisher *string `json:"publisher"`// manejo de envio para formato json
+	Publisher *string `json:"publisher"` // manejo de envio para formato json
 
+}
+
+func MigrateBook(db *gorm.DB) error {
+	err := db.AutoMigrate(&Books{})
+	return err
 }
