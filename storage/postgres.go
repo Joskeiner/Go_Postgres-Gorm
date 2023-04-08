38d0d3349180e4b13a4d3428b66306dd8f2c807c
joskeiner <joskeiner.simosa9@gmail.com>
2023-04-08 01:05:44 -0300

    implementacion de driver de postgres  para manejo de coneccion

---
+++ storage/postgres.go
@@ -0,0 +1,33 @@
+package storage
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Password string
+	User     string
+	DbName   string
+	SslMode  string
+}
+
+func NewConnection(config *Config) (*gorm.DB, error) {
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		config.Host, config.Port, config.User, config.Password, config.DbName, config.SslMode,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return db, err
+	}
+
+	return db, err
+}
