b73caaf5d9cb0899cb2abc614dd341ec9dda88c7
joskeiner <joskeiner.simosa9@gmail.com>
2023-04-08 01:02:33 -0300

    agreglo de los comentarios

--- models/Book.go
+++ models/Book.go
@@ -2,7 +2,7 @@ package models
 
 import "gorm.io/gorm"
 
-// modelo o esquema donde se manejara la informacion a guardar o extraer
+// modelo o esquema donde se manejara la informacion a guardar
 type Books struct {
 	ID uint `gorm:"primary key; autoIncrement"  json:"id"`
 
@@ -14,6 +14,7 @@ type Books struct {
 
 }
 
+// la funcion AutoMigrate migrara los datos a la base de datos y creara una table con el esquema que se expecifica en la struct
 func MigrateBook(db *gorm.DB) error {
 	err := db.AutoMigrate(&Books{})
 	return err
